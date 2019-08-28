0472f99b1cd2cd810da911c46e07c7e731a93d3c
Ire Aderinokun <iaderinokun@gmail.com>
2019-08-28 17:04:58 +0100

    Got dapp server running

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -16,7 +16,9 @@ module.exports = function(deployer) {
                             appAddress: FlightSuretyApp.address
                         }
                     }
-                    fs.writeFileSync(__dirname + '/../src/dapp/config.json',JSON.stringify(config, null, '\t'), 'utf-8');
+
+                    // @todo: change dapp-custom
+                    fs.writeFileSync(__dirname + '/../src/dapp-custom/config.json',JSON.stringify(config, null, '\t'), 'utf-8');
                     fs.writeFileSync(__dirname + '/../src/server/config.json',JSON.stringify(config, null, '\t'), 'utf-8');
                 });
     });
